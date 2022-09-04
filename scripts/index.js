fd2dd402ca4dcf88a1891682c2441b4976e7d118
Encors <cheperish@mail.ru>
2022-09-04 20:32:30 +0300

    Исправлено название блока по БЭМ

--- scripts/index.js
+++ scripts/index.js
@@ -4,13 +4,12 @@ const profileName = document.querySelector('.profile__title');
 const profileJob = document.querySelector('.profile__job');
 const closeButton = popup.querySelector('.popup__close-btn');
 const formElement = popup.querySelector('.popup__container');
-const nameInput = popup.querySelector('.popup__input_name');
-const jobInput = popup.querySelector('.popup__input_job');
+const nameInput = popup.querySelector('.popup__input_type_name');
+const jobInput = popup.querySelector('.popup__input_type_job');
 const saveButton = popup.querySelector('.popup__submit-btn');
 
 function showClick() {
 	popup.classList.add('popup_opened');
-
 	nameInput.value = profileName.textContent;
 	jobInput.value = profileJob.textContent;
 }
